7630359956977fc97ce97f538edb99e0b1be82ce
root <root@DESKTOP-51MGIR3.localdomain>
2020-07-22 17:01:52 +0200

    generating localized pages

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,14 +1,21 @@
-const axios = require("axios");
+const axios = require('axios');
+const countries = require('./countries.json')
 require('dotenv').config();
 
-module.exports = async function() {
+async function getNews(country) {
   const { API_NEWS_KEY: apiKey, API_NEWS_URL: url } = process.env;
-  const args = `?apiKey=${apiKey}&country=fr&pageSize=5`;
+  const args = `?apiKey=${apiKey}&country=${country}&pageSize=5`;
   
   try {
     const response = await axios.get(`${url}${args}`);
-    return response.data;
+    return { country, articles: response.data.articles };
   } catch (error) {
     console.error(error);
   }
+}
+
+module.exports = async function() {
+  const newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises)
+    .then(newsObjects => [].concat.apply([], newsObjects));
 };
