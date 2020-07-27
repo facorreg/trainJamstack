5ffca9f270f30cc164e5d3354c03b02cb3a5880e
root <root@DESKTOP-51MGIR3.localdomain>
2020-07-27 15:23:01 +0200

    should work

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -1,15 +1,11 @@
 const axios = require('axios');
 
 exports.handler = async function(event, _, callback) {
-  console.log('toto');
   const { WEATHER_API_URL, WEATHER_API_KEY } = process.env;
-  const { lat, long } = event.queryStringParameters;
+  const { lat, lon } = event.queryStringParameters;
   
-  const url = `${WEATHER_API_URL}?lat=${lat}&lon=${long}&units=metric&appid=${WEATHER_API_KEY}`;
-  const response = await axios.get(url).catch((err) => {
-    console.log(url);
-    throw new Error(`${err.message} ${url}`);
-  });
+  const url = `${WEATHER_API_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${WEATHER_API_KEY}`;
+  const response = await axios.get(url);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(response.data),
